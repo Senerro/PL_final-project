804a02d57f5d8a6400296b1f7e11a97189620bd7
Senerro <shadowpi1999@gmail.com>
2024-06-21 13:19:22 +0300

    rebuilt

--- newMock/src/main/java/com/example/newMock/Controller/KafkaController.java
+++ newMock/src/main/java/com/example/newMock/Controller/KafkaController.java
@@ -1,5 +1,7 @@
 package com.example.newMock.Controller;
 
+import com.example.newMock.Model.RequestDTO;
+import com.example.newMock.Model.ResponseDTO;
 import com.example.newMock.producers.KafkaProducer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 
 import com.example.newMock.Model.MessageDTO;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 @RestController
 @RequiredArgsConstructor
 public class KafkaController {
@@ -24,11 +29,11 @@ public class KafkaController {
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
-    public String sendMessage(@RequestBody MessageDTO messageDTO, @PathVariable String topic){
+    public String sendMessage(@RequestBody RequestDTO messageDTO, @PathVariable String topic){
         try {
-            log.info(messageDTO + " is sending to topic " + topic);
-            producer.sendMessageInTopic(topic, messageDTO);
-            log.debug("Message was sent to topic " + topic);
+            BigDecimal limit = getLimitByRequest(messageDTO);
+            ResponseDTO response = buildResponseByRequestData(messageDTO, limit);
+            producer.sendMessageInTopic(topic, response);
         }
         catch (Exception exception){
             return String.valueOf(ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -36,5 +41,44 @@ public class KafkaController {
         }
        return  String.valueOf(ResponseEntity.status(HttpStatus.OK)
                                             .body(messageDTO));
+    }
+
+    private BigDecimal getLimitByRequest(RequestDTO dto) {
+        return switch (dto.getClientId().charAt(0)) {
+            case '8' -> new BigDecimal(2000);
+            case '9' -> new BigDecimal(1000);
+            default -> new BigDecimal(10000);
+        };
+    }
+
+    private ResponseDTO buildResponseByRequestData(RequestDTO request, BigDecimal limit) {
+        return ResponseDTO.builder()
+                .balance(setRandomBalanceLowerThanLimit(limit))
+                .currency(setCurrencyByClientId(request))
+                .rqUID(request.getRqUID())
+                .clientId(request.getClientId())
+                .account(request.getAccount())
+                .maxLimit(String.valueOf(limit))
+                .build();
+    }
+
+    private String setCurrencyByClientId(RequestDTO request) {
+        return switch (request.getClientId().charAt(0)) {
+            case '8' -> "US";
+            case '9' -> "EU";
+            default -> "RUB";
+        };
+    }
+
+    private String setRandomBalanceLowerThanLimit(BigDecimal limit) {
+        return makeLimitedString(generateLowerBalance(limit));
+    }
+
+    private String makeLimitedString(double limit) {
+        return new DecimalFormat("#0.00").format(limit);
+    }
+
+    private double generateLowerBalance(BigDecimal limit) {
+        return Math.random() * Double.parseDouble(String.valueOf(limit));
     }
 }
--- newMock/src/main/java/com/example/newMock/producers/KafkaProducer.java
+++ newMock/src/main/java/com/example/newMock/producers/KafkaProducer.java
@@ -1,6 +1,8 @@
 package com.example.newMock.producers;
 
 import com.example.newMock.Model.MessageDTO;
+import com.example.newMock.Model.RequestDTO;
+import com.example.newMock.Model.ResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -10,9 +12,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KafkaProducer {
     @Autowired
-    private KafkaTemplate<String, MessageDTO> kafka;
+    private KafkaTemplate<String, ResponseDTO> kafka;
 
-    public void sendMessageInTopic(String topic, MessageDTO messageDTO){
+    public void sendMessageInTopic(String topic, ResponseDTO messageDTO){
         kafka.send(topic, messageDTO);
     }
 }
